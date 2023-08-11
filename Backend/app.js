52c539c4ff9128c4829f366667c99a2a56ff26ab
VincentNoc <83764899+VincentNoc@users.noreply.github.com>
2023-08-11 22:00:31 +1200

    connectDB app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const { connect } = require('mongoose');
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));  
 
