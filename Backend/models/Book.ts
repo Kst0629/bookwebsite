4a9e9b7767ecb5b52c83e5f4eb45017b9482a895
VincentNoc <83764899+VincentNoc@users.noreply.github.com>
2023-08-12 22:58:41 +1200

    starting conversion.

--- Backend/models/Book.ts
+++ Backend/models/Book.ts
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const BookSchema = new mongoose.Schema({
     title:{
@@ -28,4 +28,4 @@ const BookSchema = new mongoose.Schema({
     }
 });
 
-module.exports = Books = mongoose.model('book', BookSchema);
+module.exports = Book = mongoose.model('book', BookSchema);
